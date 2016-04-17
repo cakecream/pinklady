61e2cb9d57cd63f5e4171e164bd7e7bc4de2407b
cakecream <khanomcake_cream@hotmail.com>
2016-04-17 21:16:27 +0700

    lalalala

---
+++ f2/spw/Main.java
@@ -0,0 +1,22 @@
+package f2.spw;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Alien Attack");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(700, 650);
+		frame.getContentPane().setLayout(new BorderLayout());
+		
+		SpaceShip v = new SpaceShip(330, 550, 40, 40);
+		GamePanel gp = new GamePanel();
+		GameEngine engine = new GameEngine(gp, v);
+		frame.addKeyListener(engine);
+		frame.getContentPane().add(gp, BorderLayout.CENTER);
+		frame.setVisible(true);
+		
+		engine.start();
+	}
+}
